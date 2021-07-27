c6428df70fb6667eb5607851aebe3ccdc318006e
lrd1122 <42309826+lrd1122@users.noreply.github.com>
2021-07-27 14:49:35 +0800

    Add files via upload

---
+++ src/gx/lrd1122/CommandListener.java
@@ -0,0 +1,108 @@
+package gx.lrd1122;
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class CommandListener implements CommandExecutor{
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(strings.length < 1)
+        {
+            commandSender.sendMessage(LevelReward.colorstring("&7========" + LevelReward.config.getString("prefix") + "&7========" +
+                    "\n&a/levelreward reward <Level> 领取等级礼包 &7lr.reward" +
+                    "\n&a/levelreward reload 重载插件 &7lr.reload"));
+            return true;
+        }
+        if(strings[0].equals("reward") && strings.length == 2 && commandSender.hasPermission("lr.reward")) {
+            String getname = strings[1];
+            ConfigurationSection section = LevelReward.config.getConfigurationSection("Rewards");
+            List<String> getreward = section.getStringList(getname);
+            Player player = (Player) commandSender;
+            if(player.getLevel() >= Integer.valueOf(getname)) {
+                if(LevelReward.playerdata.get(player.getName())== null) {
+                    FileConfiguration playerdata = new YamlConfiguration();
+                    playerdata.set(player.getName(), "");
+                    try {
+                        playerdata.save(new File(LevelReward.plugin.getDataFolder(), "playerdata.yml"));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    File playerdatafile = new File(LevelReward.plugin.getDataFolder(), "playerdata.yml");
+                    LevelReward.playerdata = YamlConfiguration.loadConfiguration(playerdatafile);
+                }
+                String data = String.valueOf(LevelReward.playerdata.get(player.getName()));
+                if(!getdata(data, getname)) {
+                    player.sendMessage(data);
+                    for (int i = 0; i < getreward.size(); i++) {
+                        String getcommand = getreward.get(i);
+                        String[] commandfix = getcommand.split(":");
+                        String holder = LevelReward.PlaceholderAPI != null ? PlaceholderAPI.setPlaceholders(player, commandfix[1]) : commandfix[1];
+                        FileConfiguration playerdata = new YamlConfiguration();
+                        String dataset = data + getname + ",";
+                        playerdata.set(player.getName(), dataset);
+                        File playerdatafile = new File(LevelReward.plugin.getDataFolder(), "playerdata.yml");
+                        LevelReward.playerdata = YamlConfiguration.loadConfiguration(playerdatafile);
+                        try {
+                            playerdata.save(new File(LevelReward.plugin.getDataFolder(), "playerdata.yml"));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        if (commandfix[0].equals("player")) {
+                            player.performCommand(holder);
+                        }
+                        if (commandfix[0].equals("op")) {
+                            if (!player.isOp()) {
+                                player.setOp(true);
+                                player.performCommand(holder);
+                                player.setOp(false);
+                            } else {
+                                player.performCommand(holder);
+                            }
+                        }
+                        if(LevelReward.config.getBoolean("LevelConsume"))
+                        {
+                            player.setLevel(player.getLevel() - Integer.valueOf(getname));
+                        }
+
+                    }
+                }
+                else{
+                    player.sendMessage(LevelReward.colorstring(LevelReward.config.getString("prefix") + LevelReward.config.getString("HasRewardMessage")));
+                }
+            }
+            return true;
+        }
+        if(strings[0].equals("reload") && commandSender.hasPermission("lr.reload"))
+        {
+            LevelReward.config = LevelReward.plugin.getConfig();
+            commandSender.sendMessage(LevelReward.colorstring(LevelReward.config.getString("prefix") + "插件已重载完成"));
+            File playerdatafile = new File(LevelReward.plugin.getDataFolder(), "playerdata.yml");
+            LevelReward.playerdata = YamlConfiguration.loadConfiguration(playerdatafile);
+            return true;
+        }
+        return false;
+    }
+    private boolean getdata(String string,String current)
+    {
+        String[] data = string.split(",");
+        for(int i = 0; i< data.length; i++)
+        {
+            if(current.equals(data[i]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/gx/lrd1122/LevelReward.java
@@ -0,0 +1,48 @@
+package gx.lrd1122;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.swing.text.Document;
+import java.io.File;
+
+public class LevelReward extends JavaPlugin {
+    public static YamlConfiguration playerdata;
+    public static JavaPlugin plugin;
+    public static FileConfiguration config;
+    public static Plugin PlaceholderAPI = Bukkit.getPluginManager().getPlugin("PlaceholderAPI");
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        File playerdatafile = new File(getDataFolder(), "playerdata.yml");
+        if(playerdatafile.exists()) {
+            getLogger().info("玩家数据已成功加载");
+        }
+        else {
+            saveResource("playerdata.yml", true);
+            getLogger().info("载入玩家数据");
+        }
+        playerdata = YamlConfiguration.loadConfiguration(playerdatafile);
+        if (PlaceholderAPI != null) {
+            getLogger().info("PlaceholderAPI √");
+        } else {
+            getLogger().info("PlaceholderAPI ×");
+        }
+        Bukkit.getPluginCommand("levelreward").setExecutor(new CommandListener());
+        plugin = this;
+        config = getConfig();
+        getLogger().info("插件已加载,Bug反馈+QQ1794325461");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("插件已卸载,Bug反馈+QQ1794325461");
+    }
+    public static String colorstring(String string)
+    {
+        return string.replace("&", "§");
+    }
+}
